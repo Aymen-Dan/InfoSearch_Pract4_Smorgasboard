caf1143b744565a38636745423533a16e815dd60
Dan <stickbugdanny@gmail.com>
2024-02-21 23:11:44 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -15,8 +15,8 @@ public class Main {
 
         Scanner in = new Scanner(System.in);
 
-        System.out.println("\n0 - Show index; \n1 - Show inverted tree;\n2 - Show permutation index;\n3.Show K index in file;\n4.Search inverted tree" +
-                ";\n5.Search PI;\n6.Search KI;\n-1 - Exit\n");
+        System.out.println("\n0 - Show index; \n1 - Show inverted tree;\n2 - Show permutation index;\n3 - Show 3-gram index in file;\n4 - Search inverted tree" +
+                ";\n5 - Search permutation index;\n6 - Search 3-gram index;\n-1 - Exit\n");
         int i = in.nextInt();
 
         while(i != -1) {
@@ -35,7 +35,7 @@ public class Main {
                     break;
                 case 3:
                   tg.printThGndex();
-
+                  tg.openThreeGIndexTXT("src/results/ThreeGIndex.txt");
                     break;
                 case 4:
                     System.out.println("Enter:");
@@ -45,21 +45,22 @@ public class Main {
                     //System.out.println("Sorry! Tree Search doesn't work :(");
                     break;
                 case 5:
-                    System.out.println("Enter PI (use * in place of a chunk of word):");
+                    System.out.println("Enter Permutation Index (use * in place of a chunk of word):");
+                    in.nextLine();
                     input = in.nextLine();
                    System.out.println(pi.search(input));
                     break;
                 case 6:
-                    System.out.println("Enter KIndex:");
+                    System.out.println("Enter 3-gram Index (use * in place of a chunk of word):");
                     in.nextLine();
                     input = in.nextLine();
-                   // System.out.println(k.search(input));
+                   System.out.println(tg.search(input));
                     break;
                 default:
                     System.out.println("Option unavailable. Please choose from the list");
             }
-            System.out.println("\n0 - Show index; \n1 - Show inverted tree;\n2 - Show permutation index;\n3.Show K index in file;\n4.Search inverted tree" +
-                    ";\n5.Search PI;\n6.Search KI;\n-1 - Exit\n");
+            System.out.println("\n0 - Show index; \n1 - Show inverted tree;\n2 - Show permutation index;\n3 - Show 3-gram index in file;\n4 - Search inverted tree" +
+                    ";\n5 - Search permutation index;\n6 - Search 3-gram index;\n-1 - Exit\n");
             i = in.nextInt();
         }
 
