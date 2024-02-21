9b95e32bcfdb53ccffbb829db0a3e809d0b741a9
Dan <stickbugdanny@gmail.com>
2024-02-21 22:09:40 +0200

    Create PermutIndex.java

---
+++ src/PermutIndex.java
@@ -0,0 +1,130 @@
+
+import java.awt.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PermutIndex {
+    HashMap<String, ArrayList<String>> index;
+    Index ind;
+
+    public PermutIndex(Index ind){
+        this.ind=ind;
+        index = new HashMap<>();
+        for(String s: ind.matrix.keySet()){
+            index.put(s,permute(s));
+        }
+
+        this.saveToFile();
+    }
+
+    private ArrayList<String> permute(String input) {
+        input=input.toLowerCase();
+        ArrayList<String> res = new ArrayList<>();
+        String word = input+"$";
+        res.add(word);
+        while(!word.startsWith("$")){
+            char ch = word.charAt(0);
+            word = word.replaceFirst(String.valueOf(ch),"");
+            word = word+ch;
+            res.add(word);
+        }
+        return res;
+    }
+
+    public HashMap<String,ArrayList<Integer>> search(String input) throws Exception {
+        input = input.toLowerCase();
+        input = input.replaceAll("\\s+", "");
+
+        if(!input.matches("\\*?[\\w]+\\*?[\\w]+\\*?"))
+            throw new Exception("Incorrect format.");
+
+        HashMap<String,ArrayList<Integer>> res=new HashMap<>();
+
+        if(input.contains("*")) {
+
+            input = permuteSearch(input);
+            input = input.replaceAll("\\*","");
+
+            for (String str : index.keySet()) {
+                for(String str1 : index.get(str)) {
+                    if (str1.startsWith(input)) {
+                        res.put(str, ind.matrix.get(str));
+                        break;
+                    }
+                }
+            }
+        }else
+        {
+            res.put(input,ind.matrix.get(input));
+        }
+
+        return res;
+    }
+
+    private String permuteSearch(String input){
+        input=input.toLowerCase();
+        String res = "";
+        String word = input+"$";
+
+        while(!word.endsWith("*")){
+            char ch = word.charAt(0);
+            String s = String.valueOf(ch);
+            if(ch=='*') s ="\\*";
+            word = word.replaceFirst(s,"");
+            word = word+ch;
+        }
+        res=word;
+        return res;
+    }
+
+
+    public void printPermutIndex() {
+        for(String key : index.keySet()) {
+            System.out.println("Key: " + key);
+            ArrayList<String> values = index.get(key);
+            System.out.println("Values: " + values.toString());
+        }
+    }
+
+    /**Save to */
+    public void saveToFile() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("src/results/PermutIndex.txt"))) {
+            // Iterate through each key-value pair in the index
+            for (Map.Entry<String, ArrayList<String>> entry : index.entrySet()) {
+                String key = entry.getKey();
+                ArrayList<String> values = entry.getValue();
+
+                // Write the key and its values to the file
+                writer.write(key + ": " + values.toString() + "\n");
+            }
+
+            System.out.println("Permutation Index saved to src/results/PermutIndex.txt");
+        } catch (IOException e) {
+            System.out.println("Error saving PermutIndex to file: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void openPermutIndexTXT(String filePath) throws IOException {
+        File file = new File(filePath);
+
+        if (Desktop.isDesktopSupported()) {
+            Desktop desktop = Desktop.getDesktop();
+            System.out.println("\nOpening the file...");
+
+            if (file.exists()) {
+                desktop.open(file);
+            } else {
+                System.out.println("File not found: " + filePath + "; Please restart the program.");
+            }
+        } else {
+            System.out.println("Desktop is not supported.");
+        }
+    }
+
+}
